330170d7b2745c492c210f7cbc819f7283347f02
lee-weber <alan.weber@stud-mail.uni-wuerzburg.de>
2024-10-22 00:34:59 +0200

    .java added for communication behaviours

---
+++ src/behaviours/CyclicBehaviourPrint.java
@@ -0,0 +1,10 @@
+package behaviours;
+
+import jade.core.behaviours.CyclicBehaviour;
+
+public class CyclicBehaviourPrint extends CyclicBehaviour {
+    @Override
+    public void action() {
+
+    }
+}
